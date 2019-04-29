a9ce75d89ae89863c61fa4502f306ddff6306c3e
Mohamed Nasr <princemo4@yahoo.com>
2019-04-29 00:11:19 -0400

    Add Bootstrap and initial custom styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-facility = Facility.create!(name: Faker::Company.name)
+facility = Facility.create!(name: "Blue Alps Ski Camp")
 
 patient = Patient.create!({
                                first_name: Faker::Name.first_name,
